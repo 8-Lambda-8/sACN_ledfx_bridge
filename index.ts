433bcdc95a5d07938b10810fb1db5861fad9f08b
8-Lambda-8 <j.wasle111@gmail.com>
2024-04-06 13:45:22 +0200

    added e1.31 packet listening

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
+import { Server } from "e131";
+// config
+const sAcnUniverse = 1;
+const channel = 0;
 
+const server = new Server([sAcnUniverse]);
+
+server.on("listening", () => console.log("Started Listening"));
+
+server.on("error", console.error);
+server.on("packet-error", console.warn);
+server.on("packet-out-of-order", console.warn);
+
+server.on("packet", (inPacket: any) => {
+  let inSlotsData = inPacket.getSlotsData() as Buffer;
+});
