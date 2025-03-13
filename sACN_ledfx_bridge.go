1f7baba495ad19762682c503553dc407c85054fa
8-Lambda-8 <j.wasle111@gmail.com>
2025-03-13 19:03:01 +0100

    load config.json

--- sACN_ledfx_bridge.go
+++ sACN_ledfx_bridge.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Hundemeier/go-sacn/sacn"
 )
 
+type Config struct {
+	Universe   int      `json:"sAcnUniverse"`
+	Channel    int      `json:"channel"`
+	Scenes     []string `json:"scenes"`
+	LedFx_host string   `json:"ledfx_host"`
+	LedFx_port string   `json:"ledfx_port"`
+}
+
 func main() {
+	file, err := os.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalf("Failed to read file: %v\n", err)
+	}
+
+	var configData Config
+	err = json.Unmarshal(file, &configData)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(configData)
+
 	recv, err := sacn.NewReceiverSocket("", nil)
 	if err != nil {
 		log.Fatal(err)
